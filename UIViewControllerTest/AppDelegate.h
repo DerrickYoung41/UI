ef4820b8c948787481f75ab989577eec43261ad0
yy <Apple@cernet.edu.cn>
2016-12-06 10:40:04 +0800

    Initial Commit

---
+++ UIViewControllerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewControllerTest
+//
+//  Created by yy on 2016/12/6.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewControllerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewControllerTest
+//
+//  Created by yy on 2016/12/6.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
